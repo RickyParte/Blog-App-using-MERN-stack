118d6b815f795b9751e61bda8282df5515a3c37b
Ricky Parte <77445318+RickyParte@users.noreply.github.com>
2025-04-08 00:12:48 +0530

    Update execute.js

--- server/execute.js
+++ server/execute.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors());app.use(cors({
+    origin: 'http://3.111.139.9:3001'
+}));
 
 app.set("view engine", "ejs");
 app.use(express.json());
